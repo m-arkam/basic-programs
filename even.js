b6ca1f2c302900dd53d18156c90f9b3a02820950
m-arkam <arkamimtiaz76@gmail.com>
2024-10-05 19:30:45 +0500

    sum of arrays elements

--- even.js
+++ even.js
@@ -7,3 +7,14 @@ if(num % 2 == 0){
 
 }
 else{ console.log("num is odd", num)}
+
+   
+let b = [1,2,3,4,5];
+let result = 0;
+
+for(let i = 0; i < 5; i++){
+    result = b[i] + result;
+}
+
+
+console.log(result);
